44e1fdfc48156b312b3d0cb4e10523d20b3d6d35
Alex Shih <alex.alex.shih@gmail.com>
2020-06-07 01:09:26 -0400

    updated font to monospaced

--- scripts/balance.js
+++ scripts/balance.js
@@ -1,8 +1,8 @@
 'use strict';
 
-let balance = document.getElementById('balance');
-let pledgeAmount = document.getElementById('pledgeAmount');
-let addToFundsButton = document.getElementById('addToFundsButton');
+let balance = document.getElementById("balance");
+let pledgeAmount = document.getElementById("pledgeAmount");
+let addToFundsButton = document.getElementById("addToFundsButton");
 
 function isValidFloat(str) {
     return (/^-?[\d]*(\.[\d]+)?$/g).test(str);
@@ -13,7 +13,7 @@ addToFundsButton.onclick = function() {
     if (isValidFloat(amountToAddStr)) {
         const amountToAdd = Math.round(parseFloat(amountToAddStr) * 100)/100;
         if (amountToAdd > 0) {
-            chrome.storage.sync.get('balance', function(data) {
+            chrome.storage.sync.get("balance", function(data) {
                 let newBalance = Math.round((data.balance + amountToAdd) * 100)/100;
                 chrome.storage.sync.set({balance: newBalance}, function() {
                     updateProgressBar();
@@ -26,10 +26,10 @@ addToFundsButton.onclick = function() {
     }
 }
 
-let myBar = document.getElementById('myBar');
+let myBar = document.getElementById("myBar");
 function updateProgressBar() {
-    chrome.storage.sync.get('balance', function(balanceData) {
-        chrome.storage.sync.get('milestone', function(milestoneData) {
+    chrome.storage.sync.get("balance", function(balanceData) {
+        chrome.storage.sync.get("milestone", function(milestoneData) {
             if (balanceData.balance < milestoneData.milestone) {
                 const percentage = Math.round(balanceData.balance/milestoneData.milestone * 100);
                 myBar.style.width = percentage + "%";
