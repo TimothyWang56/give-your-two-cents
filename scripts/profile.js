d8648e083599c59f6c3b8c3a0063500aaa673d40
Alex Shih <alex.alex.shih@gmail.com>
2020-06-07 00:47:40 -0400

    updated styling and added date to donation history

--- scripts/profile.js
+++ scripts/profile.js
@@ -3,13 +3,18 @@
 function getDonationHistory() {
   chrome.storage.sync.get('donationHistory', function(data) {
     chrome.extension.getBackgroundPage().console.log('data :>> ', data);
-    data.donationHistory.forEach(element => {
-      chrome.extension.getBackgroundPage().console.log('element :>> ', element);
-      var listItem = document.createElement("div");
-      var info = document.createTextNode("I donated $" + element.amount + " to " + element.charityName);
-      listItem.appendChild(info);
-      document.getElementById('donationHistory').appendChild(listItem);
-    });
+    if (data.donationHistory.length == 0) {
+      document.getElementById('donationHistory').innerHTML = "None so far."
+    }
+    else {
+      data.donationHistory.forEach(element => {
+        chrome.extension.getBackgroundPage().console.log('element :>> ', element);
+        var listItem = document.createElement("div");
+        var info = document.createTextNode("I donated $" + element.amount + " to " + element.charityName + " at " + element.time);
+        listItem.appendChild(info);
+        document.getElementById('donationHistory').appendChild(listItem);
+      });
+    }
   })
 }
 
