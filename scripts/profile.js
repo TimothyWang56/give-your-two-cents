1cdc4415a029870ae37293d3ce119df00e270338
TimothyWang56 <timothywang56@gmail.com>
2020-06-07 00:54:12 -0400

    settled merge conflict

--- scripts/profile.js
+++ scripts/profile.js
@@ -2,15 +2,6 @@
 
 function getDonationHistory() {
   chrome.storage.sync.get('donationHistory', function(data) {
-<<<<<<< HEAD
-    data.donationHistory.forEach(element => {
-      var listItem = document.createElement("div");
-      var info = document.createTextNode("I donated $" + element.amount + " to " + element.charityName);
-      listItem.appendChild(info);
-      listItem.setAttribute("class", "historyItem");
-      document.getElementById('donationHistory').appendChild(listItem);
-    });
-=======
     chrome.extension.getBackgroundPage().console.log('data :>> ', data);
     if (data.donationHistory.length == 0) {
       document.getElementById('donationHistory').innerHTML = "None so far."
@@ -24,7 +15,6 @@ function getDonationHistory() {
         document.getElementById('donationHistory').appendChild(listItem);
       });
     }
->>>>>>> d8648e083599c59f6c3b8c3a0063500aaa673d40
   })
 }
 
